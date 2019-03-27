b44448ccf8379114be841697dc81d58cea70d4d4
Luke Abel <luke.abel@gmail.com>
2019-03-27 09:10:40 -0400

    Update rtut.sh

--- rtut.sh
+++ rtut.sh
@@ -5,10 +5,10 @@ if [[ $(tmux ls) == *$SESSION* ]]; then
   exit
 else
   tmux new -s $SESSION -n rtut -d
-  tmux send-keys -t $SESSION:0 "cd ~/repos/ruby_tutorial" C-m
+  tmux send-keys -t $SESSION:0 "cd ~/repos/ruby_tutorial/$(ls ~/repos/ruby_tutorial | sort -n | tail -n1)" C-m
 
   tmux new-window -n editor -t $SESSION
-  tmux send-keys -t $SESSION:1 "cd ~/repos/ruby_tutorial" C-m
+  tmux send-keys -t $SESSION:1 "cd ~/repos/ruby_tutorial/$(ls ~/repos/ruby_tutorial | sort -n | tail -n1)" C-m
   tmux send-keys -t $SESSION:1 "vim" C-m
 
   tmux new-window -n server -t $SESSION
