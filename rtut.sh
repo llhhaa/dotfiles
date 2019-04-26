25f15fc12d8076c9f6f70ef6f820283a1eb08859
Luke Abel <luke.abel@gmail.com>
2019-04-26 09:55:31 -0400

    Update rtut.sh

--- rtut.sh
+++ rtut.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-SESSION=workspace
+SESSION=tutorial
+LATEST=$(ls ~/repos/ruby_tutorial | sort -n | tail -n1)
 if [[ $(tmux ls) == *$SESSION* ]]; then
   tmux attach -t $SESSION
   exit
 else
-  tmux new -s $SESSION -n rtut -d
-  tmux send-keys -t $SESSION:0 "cd ~/repos/ruby_tutorial/$(ls ~/repos/ruby_tutorial | sort -n | tail -n1)" C-m
+  tmux new -s $SESSION -n "editor" -d
+  tmux send-keys -t $SESSION:0 "cd ~/repos/ruby_tutorial/$LATEST" C-m
+  tmux send-keys -t $SESSION:0 "vim" C-m
 
-  tmux new-window -n editor -t $SESSION
-  tmux send-keys -t $SESSION:1 "cd ~/repos/ruby_tutorial/$(ls ~/repos/ruby_tutorial | sort -n | tail -n1)" C-m
-  tmux send-keys -t $SESSION:1 "vim" C-m
+  tmux new-window -t $SESSION
+  tmux send-keys -t $SESSION:1 "cd ~/repos/ruby_tutorial/$LATEST" C-m
 
   tmux new-window -n server -t $SESSION
-  tmux send-keys -t $SESSION:2 "cd ~/repos/ruby_tutorial/$(ls ~/repos/ruby_tutorial | sort -n | tail -n1)" C-m
+  tmux send-keys -t $SESSION:2 "cd ~/repos/ruby_tutorial/$LATEST" C-m
   tmux send-keys -t $SESSION:2 "rails s" C-m
 
-  tmux attach -t $SESSION
+  tmux attach -t $SESSION:0
 fi
